d9e5a37bb93011e7d26bc3149cd24db5d5ca1d3b
karimhaddad <karim.haddad@ircam.fr>
2023-05-14 12:51:41 +0200

    fixed non-determinise-listener editor

--- sources/screaminterface.lisp
+++ sources/screaminterface.lisp
@@ -2,7 +2,7 @@
 
 (defvar *screamer-listener-size* (om-make-point 400 200))
 
-(defclass non-deter-window (EditorWindow);(editordindow)
+(defclass non-deter-window (EditorWindow)
   ((value-item :initform nil :accessor value-item)))
 
 (defmethod om-set-view-size ((self non-deter-window) size) 
@@ -12,87 +12,72 @@
    (when (value-item self)
      (om-set-view-size (value-item self) (om-make-point (- (w self) 25) (- (h self) 50)))))
 	 
+
 (defun non-determinise-listener (value)
   (let (dialog value-item-view)
     (setf value-item-view
           (cond 
            ((omclass-p (class-of (class-of value)))
             (setf dialog (om-make-window 'non-deter-window 
-                           :window-title "Non deterministc listener"
-                           :position :centered 
-                           :window-show nil
-                           :size *screamer-listener-size*
-                           :font *om-default-font1*
-                           :back-color (om-make-color 0.875 0.875 0.875)))
+                                         :window-title "Non deterministc listener"
+                                         :position :centered 
+                                         :window-show nil
+                                         :size *screamer-listener-size*
+                           ;:font (om-make-font "Arial" 12 :mode :srcor :style :plain)
+                                         :bg-color (om-make-color 0.875 0.875 0.875)))
             (cond
              ((Class-has-editor-p value)
               (setf (value-item dialog) 
                     (setf (editor dialog) (om-make-view (get-editor-class value)
-                                            :ref nil
-                                            :container dialog
-                                            :object value
-                                            :position (om-make-point 25 35) 
-                                            :size (om-make-point (- (w dialog) 25) (- (h dialog) 50))))))
+                                                        :ref nil
+                                                        :container dialog
+                                                        :object value
+                                                        :position (om-make-point 25 35) 
+                                                        :size (om-make-point (- (w dialog) 25) (- (h dialog) 50))))))
              
              (t (setf (value-item dialog) 
                       (setf (editor dialog) 
-                      (let* ((instance (omNG-make-new-instance value "instance"))
-                             (editor (om-make-view 'InstanceEditor
-                                       :ref nil
-                                       :container dialog
-                                       :object instance
-                                       :position (om-make-point 25 35) 
-                                       :size (om-make-point (- (w dialog) 25) (- (h dialog) 50))))
-                             (slot-boxes (make-slots-ins-boxes instance)))
-                        (setf (presentation editor) 0)
-                        (mapc #'(lambda (frame)
-                                  (omG-add-element (panel editor) frame)) slot-boxes)
-                        editor))))))
+                            (let* ((instance (omNG-make-new-instance value "instance"))
+                                   (editor (om-make-view 'InstanceEditor
+                                                         :ref nil
+                                                         :container dialog
+                                                         :object instance
+                                                         :position (om-make-point 25 35) 
+                                                         :size (om-make-point (- (w dialog) 25) (- (h dialog) 50))))
+                                   (slot-boxes (make-slots-ins-boxes instance)))
+                              (setf (presentation editor) 0)
+                              (mapc #'(lambda (frame)
+                                        (omG-add-element (panel editor) frame)) slot-boxes)
+                              editor))))))
            (t (setf dialog (om-make-window 'om-window 
-                             :window-title "Non deterministc listener"
-                             :position :centered 
-                             :window-show nil
-                             :size *screamer-listener-size*
-                             :font *om-default-font1*
-                             :bg-color (om-make-color 0.875 0.875 0.875)))
-                (om-make-dialog-item 'om-text-edit-view 
-					:size (om-make-point (- (om-point-h (om-interior-size dialog)) 40) (- (om-point-v (om-interior-size dialog)) 50)))
-				        :text (format nil "~D" value))))
-#|
-;;; OM5
-              (om-make-edit-view 'om-text-edit-view
-                                                    :save-buffer-p t
-                                                    :scrollbars :v
-                                                    :text (format nil "~D" value)
-                                                    :wrap-p t
-                                                    :size (om-make-point (- (om-point-h (om-interior-size dialog)) 40) (- (om-point-v (om-interior-size dialog)) 50))
-                                                    :position (om-make-point 25 35)))))
-;;; OM4
-              (make-instance 'scrolling-fred-view
-                :save-buffer-p t
-                :h-scrollp nil
-                :dialog-item-text (format nil "~D" value)
-                :v-scrollp t
-                :wrap-p t
-                :view-size (make-point (- (w dialog) 25) (- (h dialog) 50))
-                :view-position #@(25 35)))))
-				; :text	;:scrollbars :v ))))
-					;:save-buffer-p t
-					;:wrap-p t))))
-|#								
+                                           :window-title "Non deterministc listener"
+                                           :position :centered 
+                                           :window-show nil
+                                           :size *screamer-listener-size*
+                            ; :font (om-make-font "Arial" 12 :mode :srcor :style :plain)
+                                           :bg-color (om-make-color 0.875 0.875 0.875)))
+              (om-make-view 'om-text-edit-view
+                            :save-buffer-p t
+                            :scrollbars :v
+                            :text (format nil "~D" value)
+                            :wrap-p t
+                            :size (om-make-point (- (om-point-h (om-interior-size dialog)) 40) (- (om-point-v (om-interior-size dialog)) 50))
+                            :position (om-make-point 25 35)))))
     (om-add-subviews dialog value-item-view  
-                  (om-make-dialog-item 'om-static-text (om-make-point 10 5) (om-make-point 200 16) "Do you want another solution? "
-                                    :bg-color (om-make-color 0.624 0.624 0.624))
-                  (om-make-dialog-item 'om-button (om-make-point 220 5) (om-make-point 62 20) "No" 
-                                    :di-action (om-dialog-item-act item
-                                       (om-return-from-modal-dialog dialog nil)))
-                  (om-make-dialog-item 'om-button (om-make-point 290 5) (om-make-point 58 18) "Yes" 
-                                    :di-action (om-dialog-item-act item
-                                       (om-return-from-modal-dialog dialog t))
-                                    :default-button t))
-    (om-modal-dialog dialog)))
+                     (om-make-dialog-item 'om-static-text (om-make-point 10 5) (om-make-point 200 16) "Do you want another solution? "
+                                          :bg-color (om-make-color 0.624 0.624 0.624))
+                     (om-make-dialog-item 'om-button (om-make-point 220 5) (om-make-point 62 20) "No" 
+                                          :di-action (om-dialog-item-act item
+                                                       (om-return-from-modal-dialog dialog nil)))
+                     (om-make-dialog-item 'om-button (om-make-point 290 5) (om-make-point 58 18) "Yes" 
+                                          :di-action (om-dialog-item-act item
+                                                       (om-return-from-modal-dialog dialog t))
+                                          :default-button t))
+    dialog
+    ))
 
 ;;;(non-determinise-listener 0)
+
 
 ;;;============================================
 
