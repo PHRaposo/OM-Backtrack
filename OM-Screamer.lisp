e5a004eb5dcf68ab310ab9fd4350e6f04c508dab
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2023-05-18 06:43:13 -0300

    Update OM-Screamer.lisp

--- OM-Screamer.lisp
+++ OM-Screamer.lisp
@@ -17,7 +17,6 @@
 			 (om::om-relative-path '("sources") "screamboxes")
                          (om::om-relative-path '("sources") "screamfuns")
                          (om::om-relative-path '("sources") "screaminterface")
-                         ;(om::om-relative-path '("examples") "scream-ais") ;maybe should go in sources
                          ;(om::om-relative-path '("sources" "constraints") "constraint")
                          ;(om::om-relative-path '("sources" "constraints") "constraint-boxes")
                          ;(om::om-relative-path '("sources") "screamer-constraint-solver")	  
