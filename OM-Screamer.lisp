0079fe090a6027dc336dfec6bce4d5234a7bff85
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2023-05-18 02:11:51 -0300

    Update OM-Screamer.lisp
    Deleted om-modifs.lisp and deactivation of some files, leaving only the essential files of OM-Backtrack library.  
    I believe the constraint.lisp and constraint-boxes.lisp could be implemented later, since it was an integration between the Situation library, Om-Backtrack and local search.

--- OM-Screamer.lisp
+++ OM-Screamer.lisp
@@ -13,14 +13,14 @@
 
 (defvar *screamer-files* nil)
 (setf  *screamer-files* (list 
-                         ;(om::om-relative-path '("sources") "om-modifs")
                          (om::om-relative-path '("sources") "screamboxes")
                          (om::om-relative-path '("sources") "screamfuns")
                          (om::om-relative-path '("sources") "screaminterface")
-                         (om::om-relative-path '("examples") "scream-ais") ;maybe should go in sources
-                         (om::om-relative-path '("sources" "constraints") "constraint")
-                         (om::om-relative-path '("sources" "constraints") "constraint-boxes")
-                         (om::om-relative-path '("sources") "screamer-constraint-solver")	  
+                         (om::om-relative-path '("sources") "om-preferences")
+                         ;(om::om-relative-path '("examples") "scream-ais") ;maybe should go in sources
+                         ;(om::om-relative-path '("sources" "constraints") "constraint")
+                         ;(om::om-relative-path '("sources" "constraints") "constraint-boxes")
+                         ;(om::om-relative-path '("sources") "screamer-constraint-solver")	  
                          ;(om::om-relative-path '("sources" "closer-mop") "closer-mop-packages")
                          ;(om::om-relative-path '("sources" "closer-mop") "closer-mop-shared")
                          ;(om::om-relative-path '("sources" "closer-mop") "closer-mop-lispworks")
@@ -49,11 +49,11 @@
                                        alldiff?
                                        growing? ) Nil)
                 
-	           ("Solver" Nil Nil (screamer-solver) Nil)
-	           ("Variables" Nil Nil (an-integer-betweenv a-member-ofv an-integerv) Nil)			   
-			       ("Predicates" Nil Nil (>v ) Nil) ;+v -v *v /v <v =v /=v etc...) Nil)
-			       ("Ordering" Nil Nil (static-ordering reorder) Nil)
-			       ("Force-function" Nil Nil (linear-force divide-and-conquer-force) Nil) 
+	           ;("Solver" Nil Nil (screamer-solver) Nil)
+	           ;("Variables" Nil Nil (an-integer-betweenv a-member-ofv an-integerv) Nil)			   
+			;       ("Predicates" Nil Nil (>v ) Nil) ;+v -v *v /v <v =v /=v etc...) Nil)
+			 ;      ("Ordering" Nil Nil (static-ordering reorder) Nil)
+			  ;     ("Force-function" Nil Nil (linear-force divide-and-conquer-force) Nil) 
 
                 ;("FOLDER2" Nil Nil (package::FUNCTION) Nil)
 
