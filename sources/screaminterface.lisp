09a4e2774e64c4520b9b2d5caa08b4ac4379aba5
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2023-05-30 14:31:06 -0300

    Update screaminterface.lisp

--- sources/screaminterface.lisp
+++ sources/screaminterface.lisp
@@ -1,17 +1,25 @@
 (in-package :om)
 
-(defvar *screamer-listener-size* (om-make-point 600 300));(om-make-point 400 200))
+;(defvar *screamer-listener-size* (om-make-point 600 300));(om-make-point 400 200))
 
 (defclass non-deter-window (EditorWindow om-dialog)
   ((value-item :initform nil :accessor value-item)))
 
+#|
 (defmethod om-set-view-size ((self non-deter-window) size) 
    (declare (ignore size))
    (call-next-method) 
    ;(setf *screamer-listener-size* (om-view-size self))
    (when (value-item self)
      (om-set-view-size (value-item self) (om-make-point (- (w self) 25) (- (h self) 50)))))
-	 
+|#
+
+(defmethod oa::om-resize-callback ((self non-deter-window) x y w h)
+    (let* ((value (value-item self)))
+	;(print (list "resizef" x y w h))  
+(when value
+        (om-set-view-size value (om-make-point (- w 40) (- h 40))))
+    ))
 
 (defun non-determinise-listener (value)
   (let (dialog value-item-view)
@@ -19,10 +27,10 @@
           (cond 
            ((omclass-p (class-of (class-of value)))
             (setf dialog (om-make-window 'non-deter-window 
-                                         :window-title "Non deterministc listener"
+                                         :window-title "Non Deterministc Listener"
                                          :position :centered 
-                                         :window-show nil
-                                         :size *screamer-listener-size*
+                                         ;:window-show nil
+                                         :size (om-make-point 600 350) ;*screamer-listener-size*
                                          ;:font (om-make-font "Arial" 12 :mode :srcor :style :plain)
                                          :bg-color (om-make-color 0.875 0.875 0.875)))
             (cond
@@ -32,8 +40,8 @@
                                                         :ref nil
                                                         :container dialog
                                                         :object value
-                                                        :position (om-make-point 10 35) 
-                                                        :size (om-make-point (- (w dialog) 25) (- (h dialog) 50))))))
+                                                        :position (om-make-point 25 35) 
+                                                        :size (om-make-point (- (w dialog) 25) (- (h dialog) 25))))))
              (t (setf (value-item dialog) 
                       (setf (editor dialog) 
                             (let* ((instance (omNG-make-new-instance value "instance"))
@@ -41,8 +49,8 @@
                                                          :ref nil
                                                          :container dialog
                                                          :object instance
-                                                         :position (om-make-point 10 35) 
-                                                         :size (om-make-point (- (w dialog) 25) (- (h dialog) 50))))
+                                                         :position (om-make-point 25 35) 
+                                                         :size (om-make-point (- (w dialog) 25) (- (h dialog) 25))))
                                    (slot-boxes (make-slots-ins-boxes instance)))
                               (setf (presentation editor) 0)
                               (mapc #'(lambda (frame)
@@ -58,21 +66,29 @@
                                            ;:window-show nil
                                            ;:size *screamer-listener-size*
                                            ;:font (om-make-font "Arial" 12 :mode :srcor :style :plain)
-                                           ;:bg-color (om-make-color 0.875 0.875 0.875)))	    
-              (om-make-view 'om-text-edit-view
-                            :save-buffer-p t
-                            :scrollbars :v
-                            :text (format nil "~D" value)
-                            :wrap-p t
-                            :size (om-make-point 360 150);(- (om-point-h (om-interior-size dialog)) 40) (- (om-point-v (om-interior-size dialog)) 50))
-                            :position (om-make-point 10 35)))))
+                                           ;:bg-color (om-make-color 0.875 0.875 0.875)))
+	      (om-make-dialog-item 'om-text-edit-view
+                                   (om-make-point 25 35) (om-make-point 350 120)
+                                   (format nil "~D" value)
+                                   :font *om-default-font2* 
+                                   :bg-color *om-white-color*  
+                                   :scrollbars :v 
+                                   :wrap-p t
+                                   :save-buffer-p t)))) 	      
+              ;(om-make-view 'om-text-edit-view
+               ;             :save-buffer-p t
+                ;            :scrollbars :v
+                 ;           :text (format nil "~D" value)
+                  ;          :wrap-p t
+                   ;         :size (om-make-point 360 150);(- (om-point-h (om-interior-size dialog)) 40) (- (om-point-v (om-interior-size dialog)) 50))
+                    ;        :position (om-make-point 10 35)))))
     (om-add-subviews dialog value-item-view  
-                     (om-make-dialog-item 'om-static-text (om-make-point 10 5) (om-make-point 200 16) "Do you want another solution? "
-                                          :bg-color (om-make-color 0.624 0.624 0.624))
-                     (om-make-dialog-item 'om-button (om-make-point 220 5) (om-make-point 62 20) "No" 
+                     (om-make-dialog-item 'om-static-text (om-make-point 25 10) (om-make-point 420 200) "DO YOU WANT ANOTHER SOLUTION?")
+                                          ;:bg-color (om-make-color 0.624 0.624 0.624))
+                     (om-make-dialog-item 'om-button (om-make-point 250 5) (om-make-point 62 20) "No" 
                                           :di-action (om-dialog-item-act item
                                                        (om-return-from-modal-dialog dialog nil)))
-                     (om-make-dialog-item 'om-button (om-make-point 290 5) (om-make-point 58 18) "Yes" 
+                     (om-make-dialog-item 'om-button (om-make-point 320 5) (om-make-point 58 18) "Yes" 
                                           :di-action (om-dialog-item-act item
                                                        (om-return-from-modal-dialog dialog t))
                                           :default-button t))
@@ -89,38 +105,16 @@
   ;:owner (or owner (om-front-window) (capi:convert-to-screen)) 
   ;                        :position-relative-to :owner :x (vx dialog) :y (vy dialog)))
 
-;;;============================================
-
 (in-package :s)
 (defmacro-compile-time print-values (&body forms)
   `(catch 'succeed
      (for-effects
-       (let ((value (progn ,@forms)))
-         
+       (let ((value (progn ,@forms)))         
          (if (= om::*screamer-valuation* 2)
            (unless (om::non-determinise-listener value)
              (throw 'succeed value))
            (progn (throw 'succeed value) (print value)))))))
-#|
-;;;============================================================================== ;
-;;; ORIGINAL - FROM SCREAMER.LISP - REDEFINED IN OM-BACKTRACK LIBRARY ;;;
-		   
-(defmacro-compile-time print-values (&body expressions)
- "doc"
- ;; FIXME: Documentation lies: does not always return NIL.
- `(catch 'succeed
-    (for-effects
-      (let ((value (progn ,@expressions)))
-        (print value)
-        (unless (y-or-n-p "Do you want another solution?")
-          (throw 'succeed value))))))
 
-;;; note: Should have way of having a stream of values.
-
-(eval-when (:compile-toplevel :load-toplevel :execute) (setf *screamer?* t))
-
-;;;============================================================================= ;
-|#
 (in-package :om)
 
 
