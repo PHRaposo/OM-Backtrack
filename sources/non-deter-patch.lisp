9cfdac85292440cfc5830869eddc4802ba55123a
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2023-06-05 23:28:32 -0300

    Update non-deter-patch.lisp
    - Trying to make nondeterministic patches work in lambda mode; 
    - Disabled "non-deter-patch?" for OMLispPatch.

--- sources/non-deter-patch.lisp
+++ sources/non-deter-patch.lisp
@@ -7,7 +7,8 @@
 (let ((record (s::get-function-record (intern (string (car (list! (code self)))) :om))))
 (not (s::function-record-deterministic? record))))
 
-(defmethod non-deter-patch? ((self OMLispPatch))
+#|
+(defmethod non-deter-patch? ((self OMLispPatch)) ; needs a better solution to prevent crashes
 (if (or (null (get-lisp-exp (lisp-exp self)))
 	(and (null (first (cdr (get-lisp-exp (lisp-exp self)))))
              (equal (second (cdr (get-lisp-exp (lisp-exp self))))'(ombeep))))
@@ -16,6 +17,7 @@
               ,.(cdr (get-lisp-exp (lisp-exp self))))))
          (record (s::get-function-record fun)))
 (not (s::function-record-deterministic? record)))))
+|#
 
 (defmethod omNG-box-value ((self OMBoxPatch) &optional (num-out 0))
 (handler-bind ((error #'(lambda (c)
@@ -108,10 +110,106 @@
        (setf *let-list* oldletlist)
        (setf *lambda-context* oldlambdacontext)
 	   ))	
-(setf (compiled? self) t)))	
+(setf (compiled? self) t)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; OM-DRAW-CONTENTS
+;;; SCREAMER-PATCH-CODE
+
+(defmethod screamer-patch-code ((self OMPatch)) 
+"Prints the lisp code from a clone of a nondeterministic patch in itself mode."
+(let ((patch-clone (clone self)))
+
+(if (lisp-exp-p patch-clone)		
+  `(screamer::defun ,(intern (string (code patch-clone)) :om)
+            ;,.(cdr (get-lisp-exp (lisp-exp self)))))) ;(lisp-exp-p self))))))		
+(case *screamer-valuation*
+     (0 (s::one-value ,.(cdr (get-lisp-exp (lisp-exp patch-clone)))))
+     (1 (s::all-values ,.(cdr (get-lisp-exp (lisp-exp patch-clone)))))
+     (2 (s::print-values ,.(cdr (get-lisp-exp (lisp-exp patch-clone)))))))              						
+(let* ((boxes (boxes patch-clone))
+   (temp-out-box (find-class-boxes boxes 'OMtempOut))
+   (self-boxes (patch-has-temp-in-p patch-clone)) 
+   (out-box (find-class-boxes boxes 'OMout))
+   (in-boxes (find-class-boxes boxes 'OMin))
+   (out-symb (code patch-clone))
+   (oldletlist *let-list*)
+   (oldlambdacontext *lambda-context*)		 
+  symbols body)
+  (setf out-box (list+ temp-out-box (sort out-box '< :key 'indice)))
+  (setf in-boxes (list+ self-boxes (sort in-boxes '< :key 'indice)))
+  (setf symbols (mapcar #'(lambda (thein) (setf (in-symbol thein) (gensym))) in-boxes))
+  (setf *let-list* nil) 
+  (setf body `(case *screamer-valuation* ; => new-body
+                                 (0 (s::one-value  ,. (mapcar #'(lambda (theout)
+			               		(gen-code theout 0)) out-box)))
+                                 (1 (s::all-values   ,. (mapcar #'(lambda (theout)
+			              	 	(gen-code theout 0)) out-box)))
+                                 (2 (s::print-values  ,. (mapcar #'(lambda (theout)
+			           			(gen-code theout 0)) out-box)))))
+  (setf screamer-patchfun `(screamer::defun ,(intern (string out-symb) :om)  (,.symbols)
+                            (let* ,(reverse *let-list*) ,body)))		
+                  
+  (setf *let-list* oldletlist)
+  (setf *lambda-context* oldlambdacontext)   
+
+  screamer-patchfun ;===> returns the function code	
+ ))) 
+)	
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; SPECIAL-LAMBDA-VALUE / CURRY-LAMBDA-CODE
+;;; => ADAPTED FROM OMLOOP <= ;;;
+		 
+;screamer
+(defmethod special-lambda-value ((self OMBoxPatch) symbol)
+  "Eval a patch box in lambda mode."
+  (let* ((nesymbs nil)
+         (args  (mapcar #'(lambda (input)
+                            (if (connected? input)
+                              `',(omNG-box-value  input)
+                              (let ((newsymbol (gensym)))
+                                (push newsymbol nesymbs)
+                                newsymbol))) (inputs self))))
+    (if (null nesymbs)
+      symbol
+   (let ((screamer-patchfun (eval (screamer-patch-code (reference self))))
+         (qargs (loop for val in args collect (if (omlistp val) `',val val))) ) 
+       (eval `#'(lambda ,(reverse nesymbs)
+               (apply ',screamer-patchfun (list ,.qargs))))))))	
+		   ;`#'(lambda ,(reverse nesymbs)
+		   ;     (apply (fdefinition ',(intern (string (code (reference self))) :om)) (list ,.args)))))				   	  
+
+;screamer
+(defmethod curry-lambda-code ((self OMBoxPatch) symbol)
+ "Eval a patch box in lambda mode."
+
+ (let ((nesymbs nil)
+       (oldlambdacontext *lambda-context*))
+   (setf *lambda-context* t)
+
+(unwind-protect 	 
+ (let* ((nesymbs nil)
+        (args  (mapcar #'(lambda (input)
+                           (if (connected? input)
+                             `',(omNG-box-value  input)
+                             (let ((newsymbol (gensym)))
+                               (push newsymbol nesymbs)
+                               newsymbol))) (inputs self))))
+   (if (null nesymbs)
+     symbol
+   (let ((screamer-patchfun (eval (screamer-patch-code (reference self))))
+         (qargs (loop for val in args collect (if (omlistp val) `',val val))) ) 
+
+           `#'(lambda ,(reverse nesymbs)
+               (apply ',screamer-patchfun  (list ,.qargs))))))
+          ;`#'(lambda ,(reverse nesymbs)
+          ;    (apply (fdefinition ',(intern (string (code (reference self))) :om)) (list ,.args)))))
+ (setf *lambda-context* oldlambdacontext)	
+
+ )))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; OM-DRAW-CONTENTS - "?" - NONDETERMINISTIC PATCH
 
 #|
 (defmethod om-draw-contents :after ((self patch-finder-icon)) ; maybe not needed
@@ -134,7 +232,7 @@
                     #\?)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
-;;; TODO - OMLOOP - LISP-PATCH-CODE
+;;; TODO - OMLOOP - OMLISPPATCH
 
 		 
 
