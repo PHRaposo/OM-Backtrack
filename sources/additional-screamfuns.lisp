a0fbad90e99deb7276471eb4c575d3f9d125a19a
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2023-06-11 12:17:55 -0300

    moved to screamfuns.lisp

--- sources/additional-screamfuns.lisp
+++
@@ -1,9 +0,0 @@
-(in-package :screamer-user)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; NEW SCREAMER FUNCTIONS FOR OM 7
-
-(defun list-of-members-ofv (n dom)
-  (if (zerop n) nil
-      (cons (a-member-ofv dom)
-            (list-of-members-ofv (1- n) dom))))
