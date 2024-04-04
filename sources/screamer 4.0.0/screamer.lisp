7c703740f96b53d7602296853c6b8328c6802083
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2024-04-03 21:50:36 -0300

    Update screamer.lisp

--- sources/screamer 4.0.0/screamer.lisp
+++ sources/screamer 4.0.0/screamer.lisp
@@ -33,7 +33,8 @@
 ;;; Important notice: In this version of Screamer, if Screamer is already
 ;;; loaded and you wish to recompile the entire file, the recompilation will
 ;;; proceed much faster if you first do:
-;;; (CLRHASH SCREAMER::*FUNCTION-RECORD-TABLE*)
+
+(CLRHASH SCREAMER::*FUNCTION-RECORD-TABLE*)
 
 (in-package :screamer)
 
