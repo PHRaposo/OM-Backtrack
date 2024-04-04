b04c849bb5f00deaca48e4f19f3e924306eb3d19
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2024-04-04 11:04:47 -0300

    Update OM-Screamer.lisp

--- OM-Screamer.lisp
+++ OM-Screamer.lisp
@@ -66,7 +66,7 @@
 			             (om::om-relative-path '("sources") "screamer-solver")
 			             (om::om-relative-path '("sources") "screamer-score")					 
 			             (om::om-relative-path '("sources") "constraint-utils")	
-			             (om::om-relative-path '("sources") "analysis-tools")		 							 
+			             ;(om::om-relative-path '("sources") "analysis-tools") NOT WORKING ON WINDOWS	 							 
                           ))
 						 
 ;--------------------------------------------------
