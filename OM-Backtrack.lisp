14c37cb890b7b7cd95930559c397a1e49ad106c7
Paulo Henrique Raposo <76979664+PHRaposo@users.noreply.github.com>
2024-04-12 00:41:44 -0300

    Update OM-Backtrack.lisp

--- OM-Backtrack.lisp
+++ OM-Backtrack.lisp
@@ -25,7 +25,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; note: Since Screamer is always loaded in OM (from MathTools) I believe it's safe to do this here. 
-;(CLRHASH SCREAMER::*FUNCTION-RECORD-TABLE*)
+(CLRHASH SCREAMER::*FUNCTION-RECORD-TABLE*)
 
 (in-package :om)
 
